06dad1451ae0d1ad115989d5d5d1a2b8978c0811
Charlinatasha <charlinatasha@gmail.com>
2025-02-18 08:54:14 +1000

    imported axios and created API intergration

--- src/script.js
+++ src/script.js
@@ -1,8 +1,26 @@
+function displayQuote(response) {
+  new Typewriter("#positive-statement", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
 function generateQuote(event) {
   event.preventDefault();
+  let userInstructions = document.querySelector("#textInputElement");
+  let prompt = `User instructions: generate a personalised affirmation about${userInstructions.value}`;
+  let context =
+    "you are a kind life coach AI assistant, with years of experience providing personalised affirmations. Your affirmations are short and precise. Your mission is to provide a 5 line affirmation and seperate each line with a <br/>. Please ensure you follow the user Instructions, and sign the quote with `Your Affirmation AI` inside a <strong></strong> element after a <br/> element";
+
+  let apiKey = "f0c1a84ba5f0b6db3obaf7359402cfct";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
 
   new Typewriter("#positive-statement", {
-    strings: "Generating your personal quote....",
+    strings: "Generating your personal affirmation....",
     autoStart: true,
     cursor: "",
     delay: 1,
